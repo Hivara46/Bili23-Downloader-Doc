1102b46491d0fe513c92b4faf6e824e32c9785a5
ScottSloan <world1019@sina.com>
2026-06-01 10:56:13 +0800

    docs: 更新 v2.00.7 下载链接

--- project.data.js
+++ project.data.js
@@ -1,29 +1,18 @@
 export default {
   load() {
     return {
-      version: '2.00.6',
-      date: '2026-05-27',
-      onedrive: {
-        windows_exe_key: 'jkKsj',
-        windows_zip_key: 'k1lIL',
-        windows_7_exe_key: 'dW5h0',
-        linux_amd64_deb_key: 'xMxTz',
-        linux_amd64_tar_gz_key: 'zZbId',
-        linux_arm64_deb_key: 'G2zFB',
-        linux_arm64_tar_gz_key: '26Eix',
-        macos_aarch64_key: 'Pm4C8',
-        macos_x86_64_key: 'aOWHW'
-      },
+      version: '2.00.7',
+      date: '2026-06-01',
       lanzou: {
-        windows_exe_key: 'iHOTz3qm3csf',
-        windows_zip_key: 'iq8jD3qm3a8d',
-        windows_7_exe_key: 'ivxmG3qm3l8j',
-        linux_amd64_deb_key: 'ixAKM3qm50yb',
-        linux_amd64_tar_gz_key: 'ilpqb3qm5ebc',
-        linux_arm64_deb_key: 'i9pdb3qm55gd',
-        linux_arm64_tar_gz_key: 'iifpL3qm3h4b',
-        macos_aarch64_key: 'iYIiH3qm3sfi',
-        macos_x86_64_key: 'iZmxL3qm3wuh'
+        windows_exe_key: 'i22zS3qultqb',
+        windows_zip_key: 'iMAmr3quma1i',
+        windows_7_exe_key: 'izDZB3qulmwf',
+        linux_amd64_deb_key: 'ibsYW3quly0f',
+        linux_amd64_tar_gz_key: 'ihY6m3qum1jc',
+        linux_arm64_deb_key: 'i5eEL3qum3oj',
+        linux_arm64_tar_gz_key: 'io2Oq3qum75e',
+        macos_aarch64_key: 'iWW3a3qulqda',
+        macos_x86_64_key: 'iIJw13quls0j'
       }
     }
   }
